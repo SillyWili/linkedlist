6562c8d9b0491040b332b4e710b1ea4d44a58c55
Wiliam Muller <174868022+SillyWili@users.noreply.github.com>
2025-08-14 19:46:34 +0200

    add: new methods for linked list class
    prepend: adds a node at the end of the list
    size: returns the size of the list
    head: returns the head of the list
    tail: returns the tail of the list

--- list.js
+++ list.js
@@ -7,11 +7,10 @@ class LinkedList {
     if (this.head === null) {
       this.head = new Node();
       this.head.value = value;
-      this.head.nextNode = null;
       return;
     }
 
-    tmp = this.head;
+    let tmp = this.head;
 
     while (tmp.nextNode !== null) {
       tmp = tmp.nextNode;
@@ -19,6 +18,47 @@ class LinkedList {
     const last = new Node();
     tmp.nextNode = last;
     last.value = value;
+  }
+
+  prepend(value) {
+    if (this.head === null) {
+      this.head = new Node();
+      this.head.value = value;
+      return;
+    }
+
+    const tmp = new Node();
+    tmp.value = value;
+    tmp.nextNode = this.head;
+    this.head = tmp;
+  }
+
+  size() {
+    if (this.head === null) {
+      return 0;
+    }
+    let count = 0;
+    let tmp = this.head;
+    while (tmp !== null) {
+      count++;
+      tmp = tmp.nextNode;
+    }
+    return count;
+  }
+
+  head() {
+    return this.head;
+  }
+
+  tail() {
+    if (this.head === null) {
+      return this.head;
+    }
+    let tail = this.head;
+    while (tail.nextNode !== null) {
+      tail = tail.nextNode;
+    }
+    return tail;
   }
 }
 
