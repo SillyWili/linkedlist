95a3fe135de17c99700d36e127b1468dffe28822
Wiliam Muller <174868022+SillyWili@users.noreply.github.com>
2025-08-15 01:27:36 +0200

    add: test for all the other functions

--- main.js
+++ main.js
@@ -8,4 +8,24 @@ list.append("hamster");
 list.append("snake");
 list.append("turtle");
 
+list.prepend("rabbit");
+list.pop();
+
+let hamster = list.find("hamster");
+let head = list.Head();
+let tail = list.tail();
+
+console.log(list.toString());
+console.log(list.size());
+console.log(head, tail);
+console.log(list.at(2));
+console.log(list.toString());
+console.log(list.contains("rabbit")); // true
+console.log(list.contains("tiger")); // false
+console.log(hamster); // 4
+
+list.insertAt("lion", 2);
+console.log(list.toString());
+
+list.removeAt(6);
 console.log(list.toString());
